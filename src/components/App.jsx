e8090b049da1627822e95971f1925d66f27fb2ab
Ralph Lee <ralph.lee@live.ca>
2020-10-02 23:58:42 -0700

    Create createNotes function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import notes from "../notes";
 
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 
+function createNotes(noteItem) {
+  return (
+    <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
